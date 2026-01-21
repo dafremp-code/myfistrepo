9355918c6bf6d02dd2d0943d5d9e95070a70eb25
dafremp-code <dafremp@gmail.com>
2026-01-21 23:59:17 +0000

    Add basic calculator functionality in calculator.py
    Implement basic calculator functionality for addition and subtraction.

---
+++ calculator.py
@@ -0,0 +1,23 @@
+#A calculator built with python
+print("DEYGO CALCULATOR")
+print("Welcome!")
+print("")
+
+print("Enter 1 for addition")
+print("Enter 2 for subtraction")
+choice = int(input("What do operation do you want to do?"))
+
+if choice = 1:
+  print"Addition"
+  print("")
+  num1 = int(input("Enter the first number"))
+  num2 = int(input("Enter the second number"))
+  print(num1 + num2)
+if choice = 2:
+  print"Subtraction"
+  print("")
+  num1 = int(input("Enter the first number"))
+  num2 = int(input("Enter the second number"))
+  print(num1 - num2)
+else :
+  print ("invalid choice!")
