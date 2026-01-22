7e91c1846c6dfb7a3564778f044bc512dc22a9c3
dafremp-code <dafremp@gmail.com>
2026-01-22 00:04:58 +0000

    Fix syntax errors in calculator.py

--- calculator.py
+++ calculator.py
@@ -7,14 +7,14 @@ print("Enter 1 for addition")
 print("Enter 2 for subtraction")
 choice = int(input("What do operation do you want to do?"))
 
-if choice = 1:
-  print"Addition"
+if choice == 1:
+  print("Addition")
   print("")
   num1 = int(input("Enter the first number"))
   num2 = int(input("Enter the second number"))
   print(num1 + num2)
-if choice = 2:
-  print"Subtraction"
+if choice == 2:
+  print("Subtraction")
   print("")
   num1 = int(input("Enter the first number"))
   num2 = int(input("Enter the second number"))
